8415af34276f8a69a2d99d6197de77f9811e8410
Brandon Odiwuor <brandon.odiwuor@gmail.com>
2021-04-14 20:52:02 +0300

    Add file upload functionality

--- app/utils/utils.py
+++ app/utils/utils.py
@@ -1,7 +1,10 @@
 import jwt
-from os import environ
+from secrets import token_urlsafe
+from os import environ, path
 from functools import wraps
 from flask import session, redirect, url_for
+from werkzeug.utils import secure_filename
+
 
 def decode_token(bearer_token):
     token = bearer_token.split(" ")[1]
@@ -17,3 +20,16 @@ def login_required(view):
         return view(**kwargs)
 
     return wrapped_view
+
+
+ALLOWED_EXTENSIONS = {'pdf', 'png', 'jpg', 'jpeg'}
+
+
+def save_file(file):
+    if '.' in file.filename:
+        file_ext = file.filename.rsplit('.', 1)[1].lower()
+        if file_ext in ALLOWED_EXTENSIONS:
+            filename = secure_filename(token_urlsafe(24) + '.' + file_ext)
+            file.save(path.join(environ.get('UPLOAD_FOLDER'), filename))
+            return filename
+    return None
