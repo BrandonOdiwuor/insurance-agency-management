19ff89833e6d6caf15719ccf0dbc29182a6cd5e6
Brandon Odiwuor <brandon.odiwuor@gmail.com>
2021-04-14 20:51:14 +0300

    Update customer model

--- app/models/customer.py
+++ app/models/customer.py
@@ -1,20 +1,30 @@
 from werkzeug.security import generate_password_hash, check_password_hash
 from app.models import Base
 from app import db
-from app.utils.enums import CustomerStatus
+from app.utils.enums import CustomerStatus, GenderChoices
 
 
 class Customer(Base):
 
     __tablename__ = 'customers'
 
-    f_name = db.Column(db.String(20), nullable=False)
-    l_name = db.Column(db.String(20),  nullable=False)
-    id_no = db.Column(db.String(8), unique=True,  nullable=False)
-    email = db.Column(db.String(100), unique=True, nullable=False)
-    phone = db.Column(db.String(9), unique=True, nullable=False)
-    status = db.Column(db.Enum(CustomerStatus), nullable=False)
+    first_name = db.Column(db.String(255), nullable=False)
+    last_name = db.Column(db.String(255),  nullable=False)
+    national_id_number = db.Column(db.String(8), unique=True,  nullable=False)
+    primary_email = db.Column(db.String(100), unique=True, nullable=False)
+    primary_phone_number = db.Column(db.String(9), unique=True, nullable=False)
+    account_status = db.Column(db.Enum(CustomerStatus), nullable=False)
     password_hash = db.Column(db.String(200), nullable=False)
+    physical_address = db.Column(db.String(255), nullable=False)
+    city = db.Column(db.String(255), nullable=False)
+    county = db.Column(db.String(255), nullable=False)
+    postal_address = db.Column(db.String(255), nullable=False)
+    postal_code = db.Column(db.String(255), nullable=False)
+    gender = db.Column(db.Enum(GenderChoices), nullable=False)
+    birth_date = db.Column(db.DateTime, nullable=False)
+    kra_pin = db.Column(db.String(255), nullable=False)
+    attachment_id_front = db.Column(db.String(255), nullable=False)
+    attachment_id_back = db.Column(db.String(255), nullable=False)
     invoices = db.relationship('Invoice', backref='customer', lazy=True)
 
     @property
@@ -41,4 +51,4 @@ class Customer(Base):
         return '<Customer %r>' % (self.email)
 
     def is_active(self):
-        return self.status == CustomerStatus.ACTIVE
+        return self.account_status == CustomerStatus.ACTIVE
