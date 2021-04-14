038a6dc0c6e72b2007d6038fa4e32e52e3338a28
Brandon Odiwuor <brandon.odiwuor@gmail.com>
2021-04-14 20:53:03 +0300

    Update customer controller

--- app/controllers.py
+++ app/controllers.py
@@ -53,18 +53,28 @@ def get_items_of_sale():
 
 
 def create_customer(customer_payload):
-    new_customer = Customer(
-        f_name=customer_payload['first_name'],
-        l_name=customer_payload['last_name'],
-        id_no=customer_payload['id_no'],
-        phone=customer_payload['phone'],
-        email=customer_payload['email'],
+    customer = Customer(
+        first_name=customer_payload['first_name'],
+        last_name=customer_payload['last_name'],
+        national_id_number=customer_payload['national_id_number'],
+        primary_phone_number=customer_payload['primary_phone_number'],
+        primary_email=customer_payload['primary_email'],
         password=customer_payload['password'],
-        status=CustomerStatus.INACTIVE
+        account_status=CustomerStatus.INACTIVE,
+        physical_address=customer_payload['physical_address'],
+        city=customer_payload['city'], 
+        county=customer_payload['county'],
+        postal_address=customer_payload['postal_address'],
+        postal_code=customer_payload['postal_code'],
+        gender=customer_payload['gender'],
+        birth_date=customer_payload['birth_date'],
+        kra_pin=customer_payload['kra_pin'],
+        attachment_id_front=customer_payload['attachment_id_front'],
+        attachment_id_back = customer_payload['attachment_id_front']
     )
 
     try:
-        db.session.add(new_customer)
+        db.session.add(customer)
         db.session.commit()
 
     except Exception as exception:
@@ -103,20 +113,20 @@ def get_customer_info(customer_id):
 
 
 def validate_customer_email(email):
-    return Customer.query.filter_by(email=email).first()
+    return Customer.query.filter_by(primary_email=email).first()
 
 
 def validate_customer_telephone(telephone):
-    return Customer.query.filter_by(phone=telephone).first()
+    return Customer.query.filter_by(primary_phone_number=telephone).first()
 
 
 def update_customer_status(customer_id):
     customer = Customer.query.filter_by(id=customer_id).first()
 
-    if customer.status == CustomerStatus.ACTIVE:
-        customer.status = CustomerStatus.INACTIVE
+    if customer.account_status == CustomerStatus.ACTIVE:
+        customer.account_status = CustomerStatus.INACTIVE
     else:
-        customer.status = CustomerStatus.ACTIVE
+        customer.account_status = CustomerStatus.ACTIVE
     try:
         db.session.commit()
 
