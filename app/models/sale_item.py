a88968a98bb915ced6dc18292e6233fba31cb21e
Brandon Odiwuor <brandon.odiwuor@gmail.com>
2021-04-18 16:27:48 +0300

    Refactor sale items

--- app/models/sale_item.py
+++ app/models/sale_item.py
@@ -6,10 +6,9 @@ class SaleItem(Base):
 
     __tablename__ = 'sale_items'
 
-    name = db.Column(db.String(20))
-    category = db.Column(db.String(20))
+    name = db.Column(db.String(255), nullable=False)
+    category = db.Column(db.String(255), nullable=False)
     price = db.Column(db.Float)
-    item_quote_procedure_json = db.Column(db.JSON)
     details_json = db.Column(db.JSON)
 
     def __repr__(self):
