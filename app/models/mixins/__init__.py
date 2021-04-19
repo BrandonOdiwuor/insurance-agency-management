68af6b8f17e875c34f6c66dc476b3dbcb781bfc2
Brandon Odiwuor <brandon.odiwuor@gmail.com>
2021-04-19 20:53:26 +0300

    Add Quotes and Policy Mixins

---
+++ app/models/mixins/__init__.py
@@ -0,0 +1,14 @@
+from app import db
+from app.utils.enums import PaymentPlans, ProductTypes
+
+
+class BaseProductMixin(object):
+    product_type = db.Column(
+        db.Enum(ProductTypes), nullable=False
+    )
+    policy_start_date = db.Column(
+        db.DateTime,  default=db.func.current_timestamp()
+    )
+    sum_insured = db.Column(db.Float, nullable=False)
+    payment_plan = db.Column(db.Enum(PaymentPlans), nullable=False)
+    premium = db.Column(db.Float, nullable=False)
